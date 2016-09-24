4520b8952fd6dc19f312d68bcea0d16f65777bff
s3strm <s3strm@fastmail.com>
2016-09-24 19:55:15 +1000

    Make the run command work

---
+++ docker/run.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+_usage() {
+  echo "$0 <s3_key>" >&2
+}
+
+[[ $# -ne 1 ]] && _usage && exit 1
+
+KEY=$1
+STACK_NAME=$(../bin/get_setting MOVIE_MP4META_STACK_NAME)
+DOCKER_STACK_NAME=$(../bin/get_setting DOCKER_STACK_NAME)
+CONTAINER_NAME=$(../bin/get_setting MOVIE_MP4META_ECR_REPO)
+
+TASK_DEFINITION_ARN=$(
+  aws cloudformation describe-stacks \
+    --stack-name "${STACK_NAME}" \
+    --query 'Stacks[].Outputs[?OutputKey==`TaskDefinition`].OutputValue[]' \
+    --output "text"
+)
+
+CLUSTER_NAME=$(
+  aws cloudformation describe-stacks \
+    --stack-name "${DOCKER_STACK_NAME}" \
+    --query 'Stacks[].Outputs[?OutputKey==`Cluster`].OutputValue[]' \
+    --output "text"
+)
+
+OVERRIDES=$(cat <<EOF
+{ "containerOverrides": [
+    {
+      "name": "${CONTAINER_NAME}",
+      "environment": [
+        { "name": "KEY", "value": "${KEY}" }
+      ]
+    }
+  ]
+}
+EOF
+)
+
+aws ecs run-task \
+  --cluster ${CLUSTER_NAME} \
+  --task-definition ${TASK_DEFINITION_ARN} \
+  --overrides "${OVERRIDES}"
+
