9bdad987c45f3b62ad74a832726b5b4ea840f38c
s3strm <s3strm@fastmail.com>
2017-03-09 18:53:39 +1100

    Generate a URL for the video file

--- cfn/lambda/ffprobe.py
+++ cfn/lambda/ffprobe.py
@@ -8,15 +8,32 @@ from subprocess import PIPE
 from re import sub
 
 def _ffprobe(imdb_id, url):
+    print("Generating data for {}".format(url))
     ffprobe_path = os.getcwd() + "/bin/ffprobe"
     cmd = [ ffprobe_path, "-i", url, "-show_entries", "stream" ]
     p = Popen(cmd, stdout=PIPE, stderr=PIPE)
     out, err = p.communicate()
+    try:
+        out[0]
+    except IndexError:
+        print("ffmpeg produced no output. The error was: {}".format(err))
+
     return out
 
 def _upload(bucket, key, body):
     s3 = boto3.resource('s3')
     s3.Bucket(bucket).put_object(Key=key, Body=body, ACL="public-read")
+
+def _url(bucket, key):
+    s3 = boto3.client('s3')
+    url = s3.generate_presigned_url(
+        ClientMethod='get_object',
+        Params={
+            'Bucket': bucket,
+            'Key': key
+        }
+    )
+    return url
 
 def lambda_handler(event, context):
     for record in event["Records"]:
@@ -25,8 +42,7 @@ def lambda_handler(event, context):
             key = y["s3"]["object"]["key"]
             region = y["awsRegion"]
             imdb_id = key.split("/")[0]
-            url = "https://s3-{}.amazonaws.com/{}/{}".format(region,bucket,key)
-
+            url = _url(bucket,key)
             ffprobe_key = "{}/ffprobe.txt".format(imdb_id)
             ffprobe_body = _ffprobe(imdb_id, url)
             _upload(bucket, ffprobe_key, ffprobe_body)
