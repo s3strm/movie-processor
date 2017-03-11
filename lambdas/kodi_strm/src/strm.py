3e6f63d9868a68e7cc38e117118d8ff80f495e87
s3strm <s3strm@fastmail.com>
2017-03-12 10:29:23 +1100

    Fix bugs

--- lambdas/kodi_strm/src/strm.py
+++ lambdas/kodi_strm/src/strm.py
@@ -5,10 +5,11 @@ import json
 import os
 
 def strm(imdb_id):
-    body = "{}/url?id={}".format(os.environ("API_GATEWAY_URL"), imdb_id)
+    body = "{}/url?id={}".format(os.environ["API_GATEWAY_URL"], imdb_id)
     key = '{}/kodi.strm'.format(imdb_id)
+    print("strm is: {}".format(body))
     s3 = boto3.resource('s3')
-    s3.Bucket(os.environ("MOVIES_BUCKET")).put_object(Key=key, Body=body, ACL="private")
+    s3.Bucket(os.environ["MOVIES_BUCKET"]).put_object(Key=key, Body=body, ACL="private")
 
 def lambda_handler(event, context):
     for record in event["Records"]:
@@ -19,3 +20,9 @@ def lambda_handler(event, context):
             strm(imdb_id)
 
     return True
+
+if __name__ == "__main__":
+    with open('sample_event/tt0000000.json', 'r') as myfile:
+        sample_event_json=myfile.read()
+    event = json.loads(sample_event_json)
+    lambda_handler(event, {})
