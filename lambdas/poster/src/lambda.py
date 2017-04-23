34801237ce96b430fe0fa6e271c18affe40bcb64
s3strm <s3strm@fastmail.com>
2017-04-23 11:02:45 +1000

    Poster lambda should read SNS events

--- lambdas/poster/src/lambda.py
+++ lambdas/poster/src/lambda.py
@@ -1,6 +1,8 @@
-import sys
+import ast
 import boto3
+import json
 import os
+import sys
 
 def has_custom_poster(imdb_id):
     client = boto3.client('s3')
@@ -22,28 +24,35 @@ def poster_key(imdb_id):
 
 def lambda_handler(event,context):
     client = boto3.client('s3')
+    for record in event["Records"]:
+        for r in ast.literal_eval(record["Sns"]["Message"])["Records"]:
+            imdb_id = os.path.dirname(r["s3"]["object"]["key"])
+            print("generating poster.jpg for {}".format(imdb_id))
 
-    client.download_file(
-        os.environ["MOVIES_BUCKET"],
-        poster_key(event["imdb_id"]),
-        "/tmp/poster.jpg",
-    )
+            client.download_file(
+                os.environ["MOVIES_BUCKET"],
+                poster_key(imdb_id),
+                "/tmp/poster.jpg",
+            )
 
-    try:
-        client.upload_file(
-            "/tmp/poster.jpg",
-            os.environ["MOVIES_BUCKET"],
-            "{}/poster.jpg".format(event["imdb_id"]),
-            ExtraArgs={
-                'ContentType': 'image/jpeg',
-                'ACL': "public-read",
-            }
-        )
-    except:
-        return False
+            try:
+                client.upload_file(
+                    "/tmp/poster.jpg",
+                    os.environ["MOVIES_BUCKET"],
+                    "{}/poster.jpg".format(imdb_id),
+                    ExtraArgs={
+                        'ContentType': 'image/jpeg',
+                        'ACL': "public-read",
+                    }
+                )
+            except:
+                print("Failed to generate poster for {}".format(imdb_id))
+                continue
 
     return True
 
 if __name__ == "__main__":
-    event = { "imdb_id": "tt0000000" }
+    with open('sample_event/tt0000000.json', 'r') as myfile:
+        sample_event_json=myfile.read()
+    event = json.loads(sample_event_json)
     print(lambda_handler(event, None))
